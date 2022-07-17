1ce4f999aab6a5add14b22fee6b98a926dd8cf2a
linupy <linupy@163.com>
2022-07-17 23:27:10 +0800

    [feat] add base64 encode/decode

--- lencode/api.py
+++ lencode/api.py
@@ -1,3 +1,4 @@
+from base64 import b64encode, b64decode
 from urllib import parse
 
 
@@ -9,3 +10,15 @@ def url_encode(content: str):
 def url_decode(content: str):
     result = parse.unquote(content)
     return result
+
+
+def base64_encode(content: str):
+    st = content.encode()  # 默认以utf8编码
+    result = b64encode(st)
+    return result.decode()
+
+
+def base64_decode(content: str):
+    st = content.encode()  # 默认以utf8编码
+    result = b64decode(st)
+    return result.decode()
--- test/test_lencode.py
+++ test/test_lencode.py
@@ -6,3 +6,5 @@ if __name__ == '__main__':
     print(result)
     result = url_decode(result)
     print(result)
+    print(base64_encode('你好，世界'))
+    print(base64_decode('5L2g5aW977yM5LiW55WM'))
