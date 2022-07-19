00ffd577de16dbe6390ebd721da9d414d4e1681c
linupy <linupy@163.com>
2022-07-20 00:19:12 +0800

    v0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='u8') as fh:
 
 setuptools.setup(
     name='lencode',
-    version='0.1',
+    version='0.1.1',
     author='linupy',
     author_email='linupy@163.com',
     description='编码/加密工具包 Encoding/Encryption Toolkit',
