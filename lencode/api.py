766e7538b9502ac7b75a07b6b748204dc1be7188
linupy <linupy@163.com>
2022-07-20 22:29:43 +0800

    [feat] add md5 md5_file

--- lencode/api.py
+++ lencode/api.py
@@ -1,5 +1,6 @@
 from base64 import b64encode, b64decode
 from urllib import parse
+import hashlib
 
 
 def url_encode(content: str):
@@ -13,12 +14,24 @@ def url_decode(content: str):
 
 
 def base64_encode(content: str):
-    st = content.encode()  # 默认以utf8编码
+    st = content.encode()
     result = b64encode(st)
     return result.decode()
 
 
 def base64_decode(content: str):
-    st = content.encode()  # 默认以utf8编码
+    st = content.encode()
     result = b64decode(st)
     return result.decode()
+
+
+def md5(content: str):
+    md = hashlib.md5()
+    md.update(content.encode('u8'))
+    return md.hexdigest()
+
+
+def md5_file(file_content: bytes):
+    md = hashlib.md5()
+    md.update(file_content)
+    return md.hexdigest()
